ed61fc53d948f7740feac57cf86a2680eebfab0b
mrmelkiu <mrmelkiu@gmail.com>
2018-10-30 19:10:05 +0700

    сделал набросок классов по заданию

--- noteappUI/noteappUI/mainform.Designer.cs
+++ noteappUI/noteappUI/mainform.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace noteappUI
+﻿namespace ContactsAPPUI
 {
     partial class Mainform
     {
@@ -19,22 +19,115 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
-        private void InitializeComponent()
+        public class Program
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            /// <summary>
+            /// класс номер телефона
+            /// </summary>
+            public class NumberPh
+            {
+                private string _Number;
+                public string Number
+                {
+                    get { return _Number; }
+                    set
+                    {
+                        //спросить как лучше сделать условие
+                        if(//условие)
+                        {
+                            throw new Exception();
+                            _Number = value;
+                        }
+                    }
+                }
+            }
 
-        #endregion
-    }
+            public NumberPh(string Number)
+            {
+                Number = Number;
+            }
+
+            /// <summary>
+            /// класс конакты содержит фамилию, имя, номер, др, емайл, вкид
+            /// </summary>
+            public class contact
+            {
+                private string _surname; //как ограничить число символов
+                private string _name;
+                private string _Number;
+                private int _happyB;
+                private string _email;
+                private string _vkid;
+
+                public string surname
+                {
+                    get { return _surname; }
+                    set
+                    {
+                        _surname = value;
+                    }
+                }
+                public string name
+                {
+                    get { return _name; }
+                    set
+                    {
+                        _name = value;
+                    }
+                }
+                public string Number
+                {
+                    get { return _Number; }
+                    set
+                    {
+                        _Number = value;
+                    }
+                }
+                public int happyB
+                {
+                    get { return _happyB; }
+                    set
+                    {
+                        _happyB = value;
+                    }
+                }
+                public string email
+                {
+                    get { return _email; }
+                    set
+                    {
+                        _email = value;
+                    }
+                }
+                public string vkid
+                {
+                    get { return _vkid; }
+                    set
+                    {
+                        _vkid = value;
+                    }
+                }
+            }
+            public contact(string surname, string name, string Number, int happyB, string email, string vkid)
+            {
+                surname = surname;
+                name = name;
+                Number = Number;
+                happyB = happyB;
+                email = email;
+                vkid = vkid;
+            }
+
+            private class project
+            {
+                //хз что сюда
+            }
+
+            private class managerprj
+            {
+                //настроить сохранение
+            }
+
+        }
 }
 
