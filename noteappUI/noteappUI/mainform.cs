17605fa23323fa4246fe5023f8762a5b2e938bc1
mrmelkiu <mrmelkiu@gmail.com>
2018-10-30 19:40:03 +0700

    убрал класс паблик тк написано, что он уже содержится

--- noteappUI/noteappUI/mainform.cs
+++ noteappUI/noteappUI/mainform.cs
@@ -10,6 +10,113 @@ using System.Windows.Forms;
 
 namespace noteappUI
 {
+    /// <summary>
+    /// класс номер телефона
+    /// </summary>
+    public class NumberPh
+    {
+        private string _Number;
+        public string Number
+        {
+            get { return _Number; }
+            set
+            {
+                //спросить как лучше сделать условие
+                if (//условие)
+                        {
+                    throw new Exception();
+                    _Number = value;
+                }
+            }
+        }
+    }
+
+    public NumberPh(string Number)
+    {
+        Number = Number;
+    }
+
+    /// <summary>
+    /// класс конакты содержит фамилию, имя, номер, др, емайл, вкид
+    /// </summary>
+    public class contact
+    {
+        private string _surname; //как ограничить число символов
+        private string _name;
+        private string _Number;
+        private int _happyB;
+        private string _email;
+        private string _vkid;
+
+        public string surname
+        {
+            get { return _surname; }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Number
+        {
+            get { return _Number; }
+            set
+            {
+                _Number = value;
+            }
+        }
+        public int happyB
+        {
+            get { return _happyB; }
+            set
+            {
+                _happyB = value;
+            }
+        }
+        public string email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+            }
+        }
+        public string vkid
+        {
+            get { return _vkid; }
+            set
+            {
+                _vkid = value;
+            }
+        }
+    }
+    public contact(string surname, string name, string Number, int happyB, string email, string vkid)
+    {
+        surname = surname;
+        name = name;
+        Number = Number;
+        happyB = happyB;
+        email = email;
+        vkid = vkid;
+    }
+
+    private class project
+    {
+        //хз что сюда
+    }
+
+    private class managerprj
+    {
+        //настроить сохранение
+    }
+
     public partial class Mainform : Form
     {
         public Mainform()
